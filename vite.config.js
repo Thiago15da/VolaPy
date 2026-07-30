1bfc3a6bd32d13ac7b157972c473ae004dc798b4
Thiago15da <tescobar.15da@gmail.com>
2026-07-30 11:34:41 -0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/VolaPy/',
+  base: '/', // <--- ESTO ES LO QUE HAY QUE CAMBIAR
   plugins: [react()],
   server: {
     host: true,
